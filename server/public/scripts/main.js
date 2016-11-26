0589281f7d987a3959973c8f07cf72ff94fee49c
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-11-25 21:59:14 -1000

    three.js

--- server/public/scripts/main.js
+++ server/public/scripts/main.js
@@ -1,3 +1,5 @@
+// DELETE?
+
 require(['modules/logger'], function(logger){
   'use strict';
 
---
+++ server/public/scripts/three/trackballControls.js
@@ -0,0 +1,2 @@
+// ORIGINAL TUTORIAL: https://www.youtube.com/watch?v=4_KkHLimetQ
+var camera, controls, scene, render;
