a5315a6db2058fe5ea6e44702fd80c2e7042f408
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2019-07-23 22:46:40 -0500

    desired animation has been reached

--- server/public/scripts/three/test.js
+++ server/public/scripts/three/test.js
@@ -1,4 +1,7 @@
 const zPoint = -1000;
+let controls;
+var objectList = [];
+
 
 // Create an empty scene
 var scene = new THREE.Scene();
@@ -31,34 +34,43 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 // Append Renderer to DOM
 document.body.appendChild( renderer.domElement );
 
-// Create a Cube Mesh with basic material
 var geometry = new THREE.Geometry();
-var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
-//var cube = new THREE.Mesh( geometry, material );
-
-
 geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
 geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
 geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
 geometry.faces.push(new THREE.Face3(0, 1, 2));
 
-var material = new THREE.MeshBasicMaterial({color: 0xFF0000, side: THREE.DoubleSide});
+var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide, wireframe: true,  wireframeLinewidth: 5});
 
 var triangle = new THREE.Mesh( geometry, material );
 
-
-// Add cube to Scene
 scene.add( triangle );
+triangle.verticesNeedUpdate = true;
+triangle.futurePosition = [new THREE.Vector3(randomWidth(),randomHeight(),zPoint), new THREE.Vector3(randomWidth(),randomHeight(),zPoint), new THREE.Vector3(randomWidth(),randomHeight(),zPoint)];
+triangle.geometry.vertices
+console.log(triangle.geometry.vertices[0].x, triangle.futurePosition[0].x);
+
+function init() {
+
+};
+
+function init() {
+
+};
 
 // Render Loop
-var render = function () {
+function render() {
   requestAnimationFrame( render );
-
+  triangle.geometry.verticesNeedUpdate = true;
+  for (var i=0; i<triangle.geometry.vertices.length; i++){
+    triangle.geometry.vertices[i].x += movements(i, triangle.geometry.vertices[i].x, triangle.futurePosition[i].x);
+    triangle.geometry.vertices[i].y += movements(i, triangle.geometry.vertices[i].y, triangle.futurePosition[i].y);
+  }
   // Render the scene
   renderer.render(scene, camera);
 };
 
-
+init();
 render();
 
 function getRandomNum(min, max) {
@@ -84,4 +96,24 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function createControls() {
+  controls = new THREE.OrbitControls(camera, container)
+}
+
+function movements(i, currentPosition, futurePosition) {
+  if (Math.abs(currentPosition - futurePosition) <= 1) {
+    triangle.geometry.verticesNeedUpdate = true;
+    triangle.futurePosition[i].x = randomWidth();
+    triangle.futurePosition[i].y = randomHeight();
+    console.log(triangle.futurePosition);
+    return 0
+  } else {
+    if (currentPosition - futurePosition > 0){
+      return -1
+    } else {
+      return 1
+    }
+  }
 }
