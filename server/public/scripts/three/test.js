659628a1535c6ee471c5f63314757645cf830f68
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2019-07-24 22:52:48 -0500

    aws logo change, fixed stackoverflow hyperlink, removed blog, changed title

--- server/public/scripts/three/test.js
+++ server/public/scripts/three/test.js
@@ -1,11 +1,10 @@
-let controls;
-var objectList = [];
-
   ////////////////////////////
  // Application Parameters //
 ////////////////////////////
-const triangleCount = 1;
+const triangleCount = 15;
 const zPoint = -1000;
+let controls;
+var objectList = [];
 
 
 // Create an empty scene
@@ -13,7 +12,7 @@ var scene = new THREE.Scene();
 
 // Create a basic perspective camera
 var camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 3000 );
-//camera.position.z = 4;
+
 
 // set lights
 var light1 = new THREE.AmbientLight(0xFFFFFF, 0.5);
@@ -39,36 +38,48 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 // Append Renderer to DOM
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.Geometry();
-geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
-geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
-geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
-geometry.faces.push(new THREE.Face3(0, 1, 2));
+// var geometry = new THREE.Geometry();
+// geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
+// geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
+// geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
+// geometry.faces.push(new THREE.Face3(0, 1, 2));
+//
+// var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide, wireframe: true,  wireframeLinewidth: 5});
+//
+// var triangle = new THREE.Mesh( geometry, material );
+//
+// scene.add( triangle );
+// triangle.verticesNeedUpdate = true;
+// triangle.futurePosition = [new THREE.Vector3(randomWidth(),randomHeight(),zPoint), new THREE.Vector3(randomWidth(),randomHeight(),zPoint), new THREE.Vector3(randomWidth(),randomHeight(),zPoint)];
+// triangle.velocites = initVelocities();
 
-var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide, wireframe: true,  wireframeLinewidth: 5});
 
-var triangle = new THREE.Mesh( geometry, material );
-
-scene.add( triangle );
-triangle.verticesNeedUpdate = true;
-triangle.futurePosition = [new THREE.Vector3(randomWidth(),randomHeight(),zPoint), new THREE.Vector3(randomWidth(),randomHeight(),zPoint), new THREE.Vector3(randomWidth(),randomHeight(),zPoint)];
-triangle.velocites = initVelocities();
+for (var i=0; i<triangleCount; i++){
+  objectList.push(createTriangle())
+}
 
 
 
-// Render Loop
+render();
+
+  ///////////////
+ // Functions //
+///////////////
+
 function render() {
   requestAnimationFrame( render );
-  triangle.geometry.verticesNeedUpdate = true;
-  for (var i=0; i<triangle.geometry.vertices.length; i++){
-    triangle.geometry.vertices[i].x += movements(i, triangle.geometry.vertices[i].x, triangle.futurePosition[i].x) * triangle.velocites[i].x;
-    triangle.geometry.vertices[i].y += movements(i, triangle.geometry.vertices[i].y, triangle.futurePosition[i].y) * triangle.velocites[i].x;
+
+  for (x=0; x<triangleCount; x++){
+    objectList[x].geometry.verticesNeedUpdate = true;
+    for (var i=0; i<3; i++){
+      objectList[x].geometry.vertices[i].x += movements(x, i, objectList[x].geometry.vertices[i].x, objectList[x].futurePosition[i].x) * objectList[x].velocites[i].x;
+      objectList[x].geometry.vertices[i].y += movements(x, i, objectList[x].geometry.vertices[i].y, objectList[x].futurePosition[i].y) * objectList[x].velocites[i].x;
+    }
   }
+
   // Render the scene
   renderer.render(scene, camera);
 };
-
-render();
 
 function getRandomNum(min, max) {
     return Math.random() * (max - min) + min;
@@ -99,13 +110,13 @@ function createControls() {
   controls = new THREE.OrbitControls(camera, container)
 }
 
-function movements(i, currentPosition, futurePosition) {
+function movements(x, i, currentPosition, futurePosition) {
   if (Math.abs(currentPosition - futurePosition) <= 1) {
-    triangle.geometry.verticesNeedUpdate = true;
-    triangle.futurePosition[i].x = randomWidth();
-    triangle.futurePosition[i].y = randomHeight();
-    triangle.velocites[i] = createVelocity();
-    console.log(triangle.velocites);
+    objectList[x].geometry.verticesNeedUpdate = true;
+    objectList[x].futurePosition[i].x = randomWidth();
+    objectList[x].futurePosition[i].y = randomHeight();
+    objectList[x].velocites[i] = createVelocity();
+    console.log(objectList[x].velocites);
     return 0
   } else {
     if (currentPosition - futurePosition > 0){
@@ -124,4 +135,26 @@ function initVelocities(){
 
 function createVelocity(){
   return {x: getRandomNum(0.01,1), y: getRandomNum(0.01,1)}
+}
+
+function createTriangle(){
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
+  geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
+  geometry.vertices.push(new THREE.Vector3( randomWidth(),randomHeight(),zPoint ));
+  geometry.faces.push(new THREE.Face3(0, 1, 2));
+
+  var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide, wireframe: true,  wireframeLinewidth: 5});
+
+  var triangle = new THREE.Mesh( geometry, material );
+
+  scene.add( triangle );
+  triangle.verticesNeedUpdate = true;
+  triangle.futurePosition = [ new THREE.Vector3(randomWidth(),randomHeight(),zPoint),
+                              new THREE.Vector3(randomWidth(),randomHeight(),zPoint),
+                              new THREE.Vector3(randomWidth(),randomHeight(),zPoint)  ];
+
+  triangle.velocites = initVelocities();
+
+  return triangle
 }
