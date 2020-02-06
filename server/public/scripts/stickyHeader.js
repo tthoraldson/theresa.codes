89fe4098dbf27ac3bebcfd563a0a67839777f36f
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2020-02-05 20:49:30 -0600

    more sticky header work

--- server/public/scripts/stickyHeader.js
+++ server/public/scripts/stickyHeader.js
@@ -1,12 +1,14 @@
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("navbar");
-var sticky = header.offsetTop;
+var sticky = header.offsetTop - (header.offsetTop/10);
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
+    header.classList.remove("not-sticky");
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
+    header.classList.add("not-sticky");
   }
 }
