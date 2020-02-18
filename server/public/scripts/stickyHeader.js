a9d235b32f651f2e17ac6b42a398d1dd8b6c3f36
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2020-02-17 18:09:12 -0600

    fixed sticky header problem

--- server/public/scripts/stickyHeader.js
+++ server/public/scripts/stickyHeader.js
@@ -1,14 +1,16 @@
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("navbar");
-var sticky = header.offsetTop - (header.offsetTop/10);
+var sticky = window.innerHeight - (window.innerHeight/10);
 
 function myFunction() {
     console.log(header.offsetTop);
   if (window.pageYOffset > sticky) {
+      console.log('sticky added')
     header.classList.remove("not-sticky");
     header.classList.add("sticky");
   } else {
+      console.log('not-sticky added');
     header.classList.remove("sticky");
     header.classList.add("not-sticky");
   }
