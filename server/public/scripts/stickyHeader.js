de050d71ea0d005b5415d14a542f2427e2773af8
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2020-02-18 23:52:32 -0600

    removed debug console.logs

--- server/public/scripts/stickyHeader.js
+++ server/public/scripts/stickyHeader.js
@@ -6,11 +6,9 @@ var sticky = window.innerHeight - (window.innerHeight/10);
 function myFunction() {
     console.log(header.offsetTop);
   if (window.pageYOffset > sticky) {
-      console.log('sticky added')
     header.classList.remove("not-sticky");
     header.classList.add("sticky");
   } else {
-      console.log('not-sticky added');
     header.classList.remove("sticky");
     header.classList.add("not-sticky");
   }
