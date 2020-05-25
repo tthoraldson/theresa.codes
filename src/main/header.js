bb8a2e14310193f1d29d81b0e3e9bfea7cf8c049
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2020-05-25 14:25:50 -0500

    added a bunch of component files

---
+++ src/main/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div id="header">
+            <h1>Theresa Thoraldson</h1>
+            <h2>Machine Learning Engineer</h2>
+            </div>
+        );
+    }
+}
+
+export default Header;
