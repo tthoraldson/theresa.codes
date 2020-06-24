8da7e58176bfb0c5d4737844648db9e532564fdc
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2020-06-23 23:54:11 -0500

    building out three boilerplate

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React from 'react';
 import './vendor/reset.css';
 import './App.css';
 import Main from './main/main';
+import Three from './three/boilerplate';
+
 
 function App() {
   return (
     <div className="App">
+      <Three />
       <Main />
     </div>
   );
