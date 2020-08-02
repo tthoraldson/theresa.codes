e1586fd004a7269eb8e9709c6537c4eb27e1607e
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2020-08-01 23:21:56 -0500

    working on triangle function

--- src/three/components/objects/triangle.js
+++ src/three/components/objects/triangle.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import { useRef, useState } from 'react'
+import { mesh, Geometry, useFrame } from 'react-three-fiber'
 
 import { randomHeight } from '../../utils/random'
 
-class ThreeTriangle extends React.Component {
-    render(){
-
-        return(
-            <div>
-                <p>Henlo</p>
-            </div>
-        )
-    }
+function triangle(props) {
+    //const mesh = useRef()
+    
+    return (
+    <mesh>
+        {...props}
+        ref={mesh}
+        <Geometry args={[1, 1, 1]}/>
+    </mesh>
+    )
+    
 }
 
-export default ThreeTriangle;
+export default triangle;
