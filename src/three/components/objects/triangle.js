eb996792e7b4b640161c40ec1ace0943af2ac7cd
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2021-01-10 19:17:27 -0600

    cleaned up triangle class

--- src/three/components/objects/triangle.js
+++ src/three/components/objects/triangle.js
@@ -3,27 +3,8 @@ import { useFrame} from "react-three-fiber"
 
 function Triangle(props) {
     const mesh = useRef()
-    // const [hovered, setHover] = useState(false)
-    //const [active, setActive] = useState(false)
-
-    console.log(window.innerWidth)
-    console.log(window.innerHeight)
-    console.log(props.test)
-    
-    // useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
 
     return (
-        // <mesh
-        //     {...props}
-        //     ref={mesh}
-        //     //scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-        //     onClick={(e) => console.log('hello') }
-        //     onPointerOver={(e) => setHover(true)}
-        //     onPointerOut={(e) => setHover(false)}>
-        //     <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
-        //     <meshStandardMaterial wireframe attach="material" color={hovered ? 'white' : 'white'} />
-        // </mesh>
-
         <mesh>
         <geometry attach="geometry">
         <vector3 attachArray="vertices" args={[0, 0, 0]}></vector3>
